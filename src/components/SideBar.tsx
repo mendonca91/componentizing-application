12513391351740f8e7e6b3558d9899e2cf2adb61
Lucas Mendonca <lucas.mendonca@credihome.com.br>
2022-05-07 14:29:41 -0300

    feat: componentizing the application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,46 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+import { Button } from './Button';
+
+import { GenreResponseProps } from '../App';
+interface SideBarProps {
+  onChangeSelectedGenre: (id: number) => void;
+  selectedGenre: number;
+}
+
+export function SideBar({
+  onChangeSelectedGenre,
+  selectedGenre,
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleClickButton(id: number) {
+    onChangeSelectedGenre(id);
+  }
+
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenre === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
